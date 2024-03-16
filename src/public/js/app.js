8904d3bca0587e6df5df4b280e49af436c0c2a46
lol-chang <doclecsj@naver.com>
2024-03-17 03:31:21 +0900

    Basic real-time chat

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,5 +1,6 @@
 const messageList = document.querySelector("ul")
-const messageForm = document.querySelector("form")
+const nickForm = document.querySelector("#nick")
+const messageForm = document.querySelector("#message")
 const socket = new WebSocket(`ws://${window.location.host}`);
 
 socket.addEventListener("open", () => {
@@ -16,11 +17,24 @@ socket.addEventListener("close", () => {
     console.log("Disconnected from server");
 })
 
+function makeMessage(type, payload) {
+    const msg = {type, payload};
+    return JSON.stringify(msg);
+}
+
 function handleSubmit(event) {
     event.preventDefault();
     const input = messageForm.querySelector("input");
-    socket.send(input.value);
+    socket.send(makeMessage("new_message", input.value));
+    input.value = "";
+}
+
+function handleNickSubmit(event) {
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    socket.send(makeMessage("nickname", input.value))
     input.value = "";
 }
 
 messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
